493799c35b9b631e3c92813b0c391e9680ddf52a
zerokinetic <mhemansakkthivel@gmail.com>
2025-08-12 18:51:31 +0530

    identified insert pattern using regex

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -20,13 +22,42 @@ public class Main {
         STATEMENT_SELECT
     }
 
+    public static class Row{
+        int id;
+        String username;
+        String email;
+    }
+
     public static class Statement {
         public StatementType type;
+        public Row rowToInsert;
     }
 
     public static PrepareResult PrepareStatement(String inputBuffer, Statement statement) {
         if (inputBuffer.startsWith("insert")) {
             statement.type = StatementType.STATEMENT_INSERT;
+
+            //Note:-
+            // ^ matches the start of the line
+            // \s+ matches one or more whitespace characters
+            // (\d+) captures one or more digits as a group
+            // (\w+) captures one or more "word" characters (letters, numbers, underscore)
+            // ([^\s]+) captures one or more non-whitespace characters
+            Pattern pattern = Pattern.compile("^insert\\s+(\\d+)\\s+(\\w+)\\s+([^\\s]+)$");
+            Matcher matcher = pattern.matcher(inputBuffer);
+            if (matcher.matches()) {
+                statement.rowToInsert.id = Integer.parseInt(matcher.group(1));
+                statement.rowToInsert.username = matcher.group(2);
+                statement.rowToInsert.email = matcher.group(3);
+
+                // Print the results
+                System.out.println("ID: " + statement.rowToInsert.id);
+                System.out.println("Username: " + statement.rowToInsert.username);
+                System.out.println("Email: " + statement.rowToInsert.email);
+            } else {
+                System.out.println("PREPARE_SYNTAX_ERROR: Invalid 'insert' command format.");
+            }
+
             return PrepareResult.PREPARE_SUCCESS;
         } else if (inputBuffer.equals("select")) {
             statement.type = StatementType.STATEMENT_SELECT;
@@ -38,7 +69,7 @@ public class Main {
     public static void executeStatement(Statement statement) {
         switch (statement.type) {
             case STATEMENT_INSERT:
-                System.out.println("This is where we would do an insert.");
+//                System.out.println("This is where we would do an insert.");
                 break;
             case STATEMENT_SELECT:
                 System.out.println("This is where we would do a select.");
